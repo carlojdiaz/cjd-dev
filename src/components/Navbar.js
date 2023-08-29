c62f0f4a26b03dbc43d600522873c758431f1169
Carlo Diaz <carlodiaz139@gmail.com>
2023-08-29 17:00:37 -0400

    Setup font and styling for typography

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,44 @@
 import React from 'react';
-import { Tabs, Tab } from '@mui/material';
-import "./Navbar.css";
+import { Tabs, Tab, Grid, Typography, Box } from '@mui/material';
 import { useState } from 'react';
+import "@fontsource/outfit";
 
 function Navbar() {
 
     const [value, setValue] = useState(0);
 
-    function handleClick(val){
+    const handleClick = (event, val) => {
         setValue(val);
     }
 
     return (
-        <div className = "navbar">
-            <Tabs
-                value = {value}
-                onChange = {handleClick}
-                textColor="primary"
-                indicatorColor="secondary"
+        <Box sx={{ borderBottom: 1, borderColor: 'divider', margin: '2rem 7rem'}}>
+
+            <Grid 
+                container spacing={2} 
+                alignItems="flex-end" 
+                justifyContent={"space-around"}
             >
-                <Tab label="Home" value={0} />
-                <Tab label="About" value={1} />
-                <Tab label="Contact" value={2} />
-            </Tabs>
-        </div>
+                <Grid item>
+                    <Typography variant='h1' paddingBottom={"0.5rem"}>Carlo J Diaz</Typography>
+                </Grid>
+
+                <Grid item>
+                    <Tabs
+                        value = {value}
+                        onChange = {handleClick}
+                        textColor="primary"
+                    >
+                        <Tab label="Home" value={0} />
+                        <Tab label="About" value={1} />
+                        <Tab label="Contact" value={2} />
+                    </Tabs>
+
+                </Grid>
+                
+            </Grid>
+            
+        </Box>
     )
 }
 

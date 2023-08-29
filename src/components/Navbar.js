4a9e048c95941f45e3ef8b2f8993d245e0004555
Carlo Diaz <carlodiaz139@gmail.com>
2023-08-29 17:09:56 -0400

    Format margins and create skeleton cards

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import { Tabs, Tab, Grid, Typography, Box } from '@mui/material';
 import { useState } from 'react';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 import "@fontsource/outfit";
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
 
 function Navbar() {
 
@@ -12,7 +35,8 @@ function Navbar() {
     }
 
     return (
-        <Box sx={{ borderBottom: 1, borderColor: 'divider', margin: '2rem 7rem'}}>
+        <>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider'}}>
 
             <Grid 
                 container spacing={2} 
@@ -32,13 +56,27 @@ function Navbar() {
                         <Tab label="Home" value={0} />
                         <Tab label="About" value={1} />
                         <Tab label="Contact" value={2} />
+
                     </Tabs>
 
                 </Grid>
                 
             </Grid>
-            
         </Box>
+
+        <TabPanel value={value} index={0}>
+            <Home />
+        </TabPanel>
+
+        <TabPanel value={value} index={1}>
+            <About />
+        </TabPanel>
+
+        <TabPanel value={value} index={2}>
+            <Contact />
+        </TabPanel>
+
+        </>
     )
 }
 

5bf057f5dc116a5892ca14c6e5526b4a4116df41
Carlo Diaz <carlodiaz139@gmail.com>
2023-08-30 10:44:58 -0400

    Begin formatting each tab panel, specifically about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,32 @@
+import { Typography, Box, Grid } from '@mui/material'
 import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <Box
+        component='body'
+        sx={{ 
+            margin: '0.5rem 0rem', 
+            padding: '1rem 3rem' 
+          }}
+        >
+        <Typography variant='h2' align='center'>About Me</Typography>
+
+        <Grid
+            container spacing={3}
+            alignItems='flex-start'
+            justifyContent='space-between'
+        >
+            <Grid item>
+                <Typography variant='body1'>35o34utihejkfgdsg</Typography>
+            </Grid>
+
+            <Grid item>
+            </Grid>
+
+          
+        </Grid>
+    </Box>
   )
 }
 
